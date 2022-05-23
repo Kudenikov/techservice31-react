7dd6cf0f16349135046296cda5fe62a0bd749556
Artem Kudenikov <a.kudenikov@yandex.ru>
2022-05-23 22:39:29 +0300

    Updates

---
+++ precache-manifest.f107cb29d579c81d714a908b5ac4c0d8.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = [
+  {
+    "revision": "2e65722bc3f85b571e77",
+    "url": "/techservice31-react/static/css/main.1363a44f.chunk.css"
+  },
+  {
+    "revision": "2e65722bc3f85b571e77",
+    "url": "/techservice31-react/static/js/main.2e65722b.chunk.js"
+  },
+  {
+    "revision": "b3be8a9b1cc833812d75",
+    "url": "/techservice31-react/static/js/1.b3be8a9b.chunk.js"
+  },
+  {
+    "revision": "35ac34d3095f869ed80d",
+    "url": "/techservice31-react/static/js/2.35ac34d3.chunk.js"
+  },
+  {
+    "revision": "10bf5bbaedfdd1e9e524",
+    "url": "/techservice31-react/static/js/runtime~main.10bf5bba.js"
+  },
+  {
+    "revision": "e5490abf5fc901fe1308a6c84c6ee45a",
+    "url": "/techservice31-react/static/media/techservice-logo-1.e5490abf.png"
+  },
+  {
+    "revision": "9c78ab0d12080f44d810f2d50f2a8c19",
+    "url": "/techservice31-react/static/media/clock_yellow.9c78ab0d.svg"
+  },
+  {
+    "revision": "4451cfdfbe0707a47fdde81571384b5f",
+    "url": "/techservice31-react/static/media/checked_yellow.4451cfdf.svg"
+  },
+  {
+    "revision": "a7dd68ac4b02433a2639ca0aac990133",
+    "url": "/techservice31-react/static/media/finance.a7dd68ac.svg"
+  },
+  {
+    "revision": "c00d38626966e917d578d43e66bdd940",
+    "url": "/techservice31-react/static/media/Home.c00d3862.png"
+  },
+  {
+    "revision": "18964fa07697c903b83b454789160325",
+    "url": "/techservice31-react/static/media/Test-tube.18964fa0.png"
+  },
+  {
+    "revision": "b485c10e6c5362f92fedef5ecdb1f7db",
+    "url": "/techservice31-react/static/media/Tool.b485c10e.png"
+  },
+  {
+    "revision": "d20c921156a0736de90ca87b1c4bcba6",
+    "url": "/techservice31-react/static/media/Cloud-6.d20c9211.png"
+  },
+  {
+    "revision": "1250dec5021b513faed0b322be4890db",
+    "url": "/techservice31-react/static/media/boiler.1250dec5.png"
+  },
+  {
+    "revision": "79f530e27da8927581437e1f22ff4d0d",
+    "url": "/techservice31-react/static/media/water-analisys.79f530e2.png"
+  },
+  {
+    "revision": "c38cbf5c19ee9f5d0a69c0cb59e34b66",
+    "url": "/techservice31-react/static/media/gas-check.c38cbf5c.jpg"
+  },
+  {
+    "revision": "c7a73f98ef96701dcab90a5c1ba35364",
+    "url": "/techservice31-react/static/media/service.c7a73f98.jpg"
+  },
+  {
+    "revision": "4b3f06816033d040ef0ed60865adb2d1",
+    "url": "/techservice31-react/static/media/Rubik-Regular.4b3f0681.ttf"
+  },
+  {
+    "revision": "11598c28bd4c62d359b58d8a810f385f",
+    "url": "/techservice31-react/static/media/Rubik-Bold.11598c28.ttf"
+  },
+  {
+    "revision": "043112d9986804e8f6fbb0b9924ee8be",
+    "url": "/techservice31-react/static/media/Rubik-ExtraBold.043112d9.ttf"
+  },
+  {
+    "revision": "f2b064ffc4aa426ea2e2b81bd4662a63",
+    "url": "/techservice31-react/static/media/Rubik-Black.f2b064ff.ttf"
+  },
+  {
+    "revision": "09205f1efbe55a9102ec1d9fed865914",
+    "url": "/techservice31-react/static/media/background-main.09205f1e.jpg"
+  },
+  {
+    "revision": "e734e376593b17e856b60021992fda8e",
+    "url": "/techservice31-react/static/media/background-sequence.e734e376.jpg"
+  },
+  {
+    "revision": "f232945e00a9800ffdfb510e3a636a4a",
+    "url": "/techservice31-react/index.html"
+  }
+];
